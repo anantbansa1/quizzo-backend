acde5e71a35be9dd3c384c6653561f08eba82085
anant <ab6157@dseu.ac.in>
2024-12-09 14:46:50 +0530

    [hotfix]: DB connection

--- src/config/ormconfig.pgap.ts
+++ src/config/ormconfig.pgap.ts
@@ -1,42 +1,6 @@
 import { DataSource, DataSourceOptions } from "typeorm";
-import {
-  VisibleAlphaCorrelationRecommendation,
-  VisibleAlphaStandardMetric,
-} from "~src/svc/modules/correlations";
-import {
-  Document,
-  DocumentData,
-  DocumentInsight,
-  DocumentSummary,
-} from "~src/svc/modules/documents";
-import { EarningsCallSchedule } from "~src/svc/modules/ec-state-machine/entities";
-import { QuantInsight, QuantInsightTrend } from "~src/svc/modules/insights";
-import { Keyword, KeywordSuggestion } from "~src/svc/modules/keywords";
-import { CuratedManagementQuestion } from "~src/svc/modules/management-questions";
-import {
-  QuantReadThrough,
-  Segment,
-  SegmentData,
-  SegmentRelationData,
-  SegmentRelationTrend,
-  StockSegmentRelation,
-} from "~src/svc/modules/read-throughs";
-import {
-  Etf,
-  Industry,
-  Sector,
-  Stock,
-  StockEtfRelation,
-  StockTopology,
-  StockTopologyMember,
-} from "~src/svc/modules/stocks";
-import { VaMetric } from "~src/svc/modules/stocks/entities/va-metrics";
+import { Exam, ExamResponse } from "~src/svc/modules/exam/entities";
 import { User } from "~src/svc/modules/users";
-import { Guidance } from "~src/svc/modules/guidance";
-import { VaMetricsMetadata } from "~src/svc/modules/stocks/entities/va-metrics-metadata";
-import { SectorTrend } from "~src/svc/modules/sector-trends/entities";
-import { SectorIndustryEntity } from "~src/svc/modules/stocks/entities/sector_industry_entity";
-import { WebSearchHistory } from "~src/svc/modules/web-search";
 
 const config: DataSourceOptions = {
   type: "postgres",
@@ -56,40 +20,7 @@ const config: DataSourceOptions = {
   }),
   connectTimeoutMS: 15 * 1000,
   maxQueryExecutionTime: 60 * 1000,
-  entities: [
-    Stock,
-    User,
-    Industry,
-    Sector,
-    Document,
-    DocumentData,
-    DocumentInsight,
-    DocumentSummary,
-    StockTopology,
-    VisibleAlphaStandardMetric,
-    StockTopologyMember,
-    QuantReadThrough,
-    CuratedManagementQuestion,
-    QuantInsight,
-    QuantInsightTrend,
-    VisibleAlphaCorrelationRecommendation,
-    Keyword,
-    Etf,
-    StockEtfRelation,
-    Segment,
-    StockSegmentRelation,
-    SegmentRelationData,
-    SegmentData,
-    SegmentRelationTrend,
-    EarningsCallSchedule,
-    VaMetric,
-    VaMetricsMetadata,
-    Guidance,
-    KeywordSuggestion,
-    SectorTrend,
-    SectorIndustryEntity,
-    WebSearchHistory,
-  ],
+  entities: [Exam, User, ExamResponse],
   migrations: ["dist/src/svc/migrations/*.{ts,js}"],
   migrationsTableName: "migrations_typeorm",
   migrationsRun: false,

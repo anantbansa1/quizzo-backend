e6adbc6763b79a64eab93331a3ed25e00455dc0f
anant <ab6157@dseu.ac.in>
2024-12-12 01:08:25 +0530

    [hotfix]: servere crash fix

--- src/svc/modules/exam/routers.ts
+++ src/svc/modules/exam/routers.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { asyncMiddleware } from "~src/svc/modules/common/middlewares/async";
 import {
   attempt,
   createExam,
@@ -11,10 +12,10 @@ import {
 
 export const examRouter = express.Router();
 
-examRouter.post("/", createExam);
-examRouter.post("/update", updateExam);
-examRouter.get("/", getExam);
-examRouter.post("/attempt", attempt);
-examRouter.get("/results", getResults);
-examRouter.get("/all", getAllExams);
-examRouter.post("/delete", deleteExam);
+examRouter.post("/", asyncMiddleware(createExam));
+examRouter.post("/update", asyncMiddleware(updateExam));
+examRouter.get("/", asyncMiddleware(getExam));
+examRouter.post("/attempt", asyncMiddleware(attempt));
+examRouter.get("/results", asyncMiddleware(getResults));
+examRouter.get("/all", asyncMiddleware(getAllExams));
+examRouter.post("/delete", asyncMiddleware(deleteExam));
